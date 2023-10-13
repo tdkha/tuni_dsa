ec47d3cce9c53dd9821a6796e68f97a11b53102e
KHA TIEU <kha.tieu@student.lab.fi>
2023-10-14 02:34:39 +0300

    try-catch wk01_asg1

--- wk07/stl/stl1.cpp
+++ wk07/stl/stl1.cpp
@@ -6,20 +6,22 @@
 
 using namespace std;
 
-
 /**
  * @brief sort a given vector to an ascending order
  *
  * @param v vector to be sorted
  * @return int EXIT_SUCCESS if everything went OK, EXIT_FAILURE otherwise
  */
-int sortAsc(std::vector<int>& v)
+int sortAsc(std::vector<int> &v)
 {
-    try{
-       std::sort(v.begin(), v.end(),[](int a, int b) { return a < b; });
-        return EXIT_FAILURE;
-    }catch(...){
+    try
+    {
+        std::sort(v.begin(), v.end(), [](int a, int b)
+                  { return a < b; });
         return EXIT_SUCCESS;
-    }    
+    }
+    catch (...)
+    {
+        return EXIT_FAILURE;
+    }
 }
-
